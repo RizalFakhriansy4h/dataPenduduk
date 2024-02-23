53f5e7404ab785a989556940b9fa82c39765e495
rizalFakhri00 <rizalludinfakhriansyah@gmail.com>
2024-02-23 16:45:59 +0700

    ubah untuk

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'https://rizalfakhri.online/ci_rizal/';
-// $config['base_url'] = 'http://localhost/data_kependudukan/';
+// $config['base_url'] = 'https://rizalfakhri.online/ci_rizal/';
+$config['base_url'] = 'http://localhost/data_kependudukan/';
 
 /*
 |--------------------------------------------------------------------------
